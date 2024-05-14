3175047e6dbefb449e0226ab72a75383112100b5
caiman48 <caiman48clm@gmail.com>
2024-05-14 19:59:23 -0400

    starting again , structure and navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/navbar/navbar'
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
-  );
-};
+    <div>
+<Navbar/>
+    </div>
+  )
+}
 
-export default App;
+export default App
