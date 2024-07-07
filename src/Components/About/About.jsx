39fee2044844f42a06139d173de4326344998989
caiman48 <caiman48clm@gmail.com>
2024-07-07 11:43:35 -0400

    changed few styles

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,12 +4,20 @@ import logo from '../../assets/logo.png';
 const About = () => {
   return (
     <div className='about' id="about">
-      <h2>About Me</h2>
+      <h2>About Us</h2>
       <div className='about-content'>
-        <img src={logo} alt='Profile' />
-        <p>
-          Hello! I am  Manny simpre llego tarde , pero mis trabajos son expectaculares 
-        </p>
+        <img src={logo} alt='Profile' className='about-logo' />
+        <div className='about-text'>
+          <p>
+            Welcome to Manny&apos;s Stonework! Established two years ago, we have rapidly expanded our services to bring exceptional stonework to our community. Our commitment to quality and craftsmanship has made us a trusted name in the industry.
+          </p>
+          <p>
+            At Manny&apos;s Stonework, we specialize in a variety of stonework projects, from elegant garden walls to modern stone facades. Our team of skilled artisans is dedicated to transforming your creative vision into reality. We take pride in our attention to detail and our ability to deliver outstanding results on time.
+          </p>
+          <p>
+            Join us on this exciting journey as we continue to grow and bring more beautiful and durable stonework to our valued clients.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,38 +1,40 @@
 import './Hero.css';
 import Rustica_img from "../../assets/Rustica.png";
-import project1_img from "../../assets/project1.png"; // Asegúrate de tener esta imagen en la carpeta assets
-import project2_img from "../../assets/Imagen2.png"; // Asegúrate de tener esta imagen en la carpeta assets
+import project1_img from "../../assets/project1.png"; 
+import project2_img from "../../assets/Imagen2.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer, faBuilding, faGlassCheers } from '@fortawesome/free-solid-svg-icons';
 
 const Hero = () => {
   return (
     <div className="Hero">
-    <div className="hero-image1-container">
-      <img src={Rustica_img} className="hero-image1" alt="Rustica" />
-    </div>
-    <div className="text-section">
-      <h2 className="text-title">
-        DESIGN <span className="icon">🌿</span> BUILD <span className="icon">🌿</span> ENJOY
-      </h2>
-      <p className="text-subtitle">Bringing Your Creative Vision to Life</p>
-    </div>
-    <div className="projects-container">
-      <div className="project-item">
-        <img src={project1_img} alt="Project 1" className="project-image" />
-        <div className="project-content">
-          <h2>Modern Stone Facade</h2>
-          <p>This project showcases a modern stone facade for a residential property, featuring clean lines and a combination of natural stone textures that provide a contemporary look.</p>
+      <div className="hero-image1-container">
+        <img src={Rustica_img} className="hero-image1" alt="Rustica" />
+      </div>
+      <div className="text-section">
+        <h2 className="text-title">
+          CRAFT <FontAwesomeIcon icon={faHammer} className="icon" /> CONSTRUCT <FontAwesomeIcon icon={faBuilding} className="icon" /> CELEBRATE <FontAwesomeIcon icon={faGlassCheers} className="icon" />
+        </h2>
+        <p className="text-subtitle">Where Quality Meets Craftsmanship</p>
+      </div>
+      <div className="projects-container">
+        <div className="project-item">
+          <img src={project1_img} alt="Project 1" className="project-image" />
+          <div className="project-content">
+            <h2>Modern Stone Facade</h2>
+            <p>This project showcases a modern stone facade for a residential property, featuring clean lines and a combination of natural stone textures that provide a contemporary look.</p>
+          </div>
+        </div>
+        <div className="project-item">
+          <div className="project-content">
+            <h2>Elegant Garden Wall</h2>
+            <p>We built an elegant garden wall using carefully selected stones to blend seamlessly with the surrounding landscape, enhancing the natural beauty of the garden space.</p>
+          </div>
+          <img src={project2_img} alt="Project 2" className="project-image" />
         </div>
       </div>
-      <div className="project-item">
-        <div className="project-content">
-          <h2>Elegant Garden Wall</h2>
-          <p>We built an elegant garden wall using carefully selected stones to blend seamlessly with the surrounding landscape, enhancing the natural beauty of the garden space.</p>
-        </div>
-        <img src={project2_img} alt="Project 2" className="project-image" />
-      </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Hero;
