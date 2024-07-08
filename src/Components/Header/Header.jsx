90f1c22e9faf48c1bc00f5e619c9c76a4653ab3c
caiman48 <caiman48clm@gmail.com>
2024-07-08 13:07:42 -0400

    3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,11 +16,16 @@ const Header = ({ currentPage, handlePageChange }) => {
       <div className="logo-container">
         <img src={logo} alt="Manny's Stonework" className="logo" />
       </div>
-      <div className="nav-container">
-        <div className="hamburger-menu" onClick={toggleMenu}>
-          ☰
-        </div>
-        <ul className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+      <div className="phone-number">
+        <a href="tel:+1234567890">
+          <span className="phone-icon">📞</span> +1 (234) 567-890
+        </a>
+      </div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        ☰
+      </div>
+      <div className={`nav-container ${isMenuOpen ? 'open' : ''}`}>
+        <ul className="nav-menu">
           <li>
             <a
               href="#hero"
@@ -58,11 +63,6 @@ const Header = ({ currentPage, handlePageChange }) => {
             </a>
           </li>
         </ul>
-      </div>
-      <div className="phone-number">
-        <a href="tel:+1234567890">
-          <span className="phone-icon">📞</span> +1 (234) 567-890
-        </a>
       </div>
     </div>
   );
