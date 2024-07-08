806f5f1ce29164d2462b7a0a5aa82da2da0ed85f
caiman48 <caiman48clm@gmail.com>
2024-07-08 14:30:51 -0400

    6

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,13 @@
-import './Hero.css';
+import "./Hero.css";
 import Rustica_img from "../../assets/Rustica.png";
-import project1_img from "../../assets/project1.png"; 
+import project1_img from "../../assets/project1.png";
 import project2_img from "../../assets/Imagen2.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHammer, faBuilding, faGlassCheers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHammer,
+  faBuilding,
+  faGlassCheers,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => {
   return (
@@ -13,7 +17,9 @@ const Hero = () => {
       </div>
       <div className="text-section">
         <h2 className="text-title">
-          CRAFT <FontAwesomeIcon icon={faHammer} className="icon" /> CONSTRUCT <FontAwesomeIcon icon={faBuilding} className="icon" /> CELEBRATE <FontAwesomeIcon icon={faGlassCheers} className="icon" />
+          CRAFT <FontAwesomeIcon icon={faHammer} className="icon" /> CONSTRUCT{" "}
+          <FontAwesomeIcon icon={faBuilding} className="icon" /> CELEBRATE{" "}
+          <FontAwesomeIcon icon={faGlassCheers} className="icon" />
         </h2>
         <p className="text-subtitle">Where Quality Meets Craftsmanship</p>
       </div>
@@ -22,13 +28,21 @@ const Hero = () => {
           <img src={project1_img} alt="Project 1" className="project-image" />
           <div className="project-content">
             <h2>Modern Stone Facade</h2>
-            <p>This project showcases a modern stone facade for a residential property, featuring clean lines and a combination of natural stone textures that provide a contemporary look.</p>
+            <p>
+              This project showcases a modern stone facade for a residential
+              property, featuring clean lines and a combination of natural stone
+              textures that provide a contemporary look.
+            </p>
           </div>
         </div>
         <div className="project-item">
           <div className="project-content">
             <h2>Elegant Garden Wall</h2>
-            <p>We built an elegant garden wall using carefully selected stones to blend seamlessly with the surrounding landscape, enhancing the natural beauty of the garden space.</p>
+            <p>
+              We built an elegant garden wall using carefully selected stones to
+              blend seamlessly with the surrounding landscape, enhancing the
+              natural beauty of the garden space.
+            </p>
           </div>
           <img src={project2_img} alt="Project 2" className="project-image" />
         </div>
