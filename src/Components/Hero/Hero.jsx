c575a838fb8312f83f01e42288e2681a268e8829
caiman48 <caiman48clm@gmail.com>
2024-07-08 17:11:02 -0400

    added neww pictures

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -25,26 +25,22 @@ const Hero = () => {
       </div>
       <div className="projects-container">
         <div className="project-item">
-          <img src={project1_img} alt="Project 1" className="project-image" />
+          <img src={project1_img} alt="Modern Stone Pool Facade" className="project-image" />
           <div className="project-content">
-            <h2>Modern Stone Facade</h2>
+            <h2>Modern Stone Pool Facade</h2>
             <p>
-              This project showcases a modern stone facade for a residential
-              property, featuring clean lines and a combination of natural stone
-              textures that provide a contemporary look.
+              This stunning pool features a modern stone facade, designed to complement the elegance of the residential setting. The natural stone finishes and clean lines create a contemporary and sophisticated look, offering a perfect space for relaxation and enjoyment.
             </p>
           </div>
         </div>
         <div className="project-item">
           <div className="project-content">
-            <h2>Elegant Garden Wall</h2>
+          <h2>Elegant Outdoor Fireplace</h2>
             <p>
-              We built an elegant garden wall using carefully selected stones to
-              blend seamlessly with the surrounding landscape, enhancing the
-              natural beauty of the garden space.
+              This exquisite outdoor fireplace is crafted with meticulously selected stones, seamlessly blending with the surrounding landscape. Designed to enhance the natural beauty of the garden, this structure provides both a functional and aesthetically pleasing focal point for outdoor gatherings.
             </p>
           </div>
-          <img src={project2_img} alt="Project 2" className="project-image" />
+          <img src={project2_img} alt="Elegant Garden Wall" className="project-image" />
         </div>
       </div>
     </div>
