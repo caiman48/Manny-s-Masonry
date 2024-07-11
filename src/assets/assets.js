a06fc3dc54393b470cfd40dfeed9ddf227a5c3d9
caiman48 <caiman48clm@gmail.com>
2024-07-11 16:11:49 -0400

    added assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,117 @@
+import project1Preview from './project1.png';
+import project1_1 from './project1/project1_1.png';
+import project1_2 from './project1/project1_2.png';
+import project1_3 from './project1/project1_3.png';
+import project1_4 from './project1/project1_4.png';
+import project1_5 from './project1/project1_5.png';
+import project1_6 from './project1/project1_6.png';
+import project1_7 from './project1/project1_7.png';
+import project1_8 from './project1/project1_8.png';
+import project1_9 from './project1/project1_9.png';
+import project1_10 from './project1/project1_10.png';
+
+import project2Preview from './project2.png';
+import project2_1 from './project2/project2_1.png';
+import project2_2 from './project2/project2_2.png';
+import project2_3 from './project2/project2_3.png';
+import project2_4 from './project2/project2_4.png';
+import project2_5 from './project2/project2_5.png';
+import project2_6 from './project2/project2_6.png';
+import project2_7 from './project2/project2_7.png';
+
+import project3Preview from './project3.png';
+import project3_1 from './project3/project3_1.png';
+import project3_2 from './project3/project3_2.png';
+import project3_3 from './project3/project3_3.png';
+import project3_4 from './project3/project3_4.png';
+import project3_5 from './project3/project3_5.png';
+import project3_6 from './project3/project3_6.png';
+
+import project4Preview from './project4.png';
+import project4_1 from './project4/project4_1.png';
+import project4_2 from './project4/project4_2.png';
+import project4_3 from './project4/project4_3.png';
+import project4_4 from './project4/project4_4.png';
+import project4_5 from './project4/project4_5.png';
+import project4_6 from './project4/project4_6.png';
+import project4_7 from './project4/project4_7.png';
+
+import project5Preview from './project5.png';
+import project5_1 from './project5/project5_1.png';
+import project5_2 from './project5/project5_2.png';
+import project5_3 from './project5/project5_3.png';
+import project5_4 from './project5/project5_4.png';
+import project5_5 from './project5/project5_5.png';
+import project5_6 from './project5/project5_6.png';
+
+import project6Preview from './project6.png';
+import project6_1 from './project6/project6_1.png';
+import project6_2 from './project6/project6_2.png';
+import project6_3 from './project6/project6_3.png';
+import project6_4 from './project6/project6_4.png';
+import project6_5 from './project6/project6_5.png';
+import project6_6 from './project6/project6_6.png';
+import project6_7 from './project6/project6_7.png';
+
+const project1Images = [
+  project1_1, project1_2, project1_3, project1_4, project1_5,
+  project1_6, project1_7, project1_8, project1_9, project1_10
+];
+
+const project2Images = [
+  project2_1, project2_2, project2_3, project2_4, project2_5,
+  project2_6, project2_7
+];
+
+const project3Images = [
+  project3_1, project3_2, project3_3, project3_4, project3_5,
+  project3_6
+];
+
+const project4Images = [
+  project4_1, project4_2, project4_3, project4_4, project4_5,
+  project4_6, project4_7
+];
+
+const project5Images = [
+  project5_1, project5_2, project5_3, project5_4, project5_5,
+  project5_6
+];
+
+const project6Images = [
+  project6_1, project6_2, project6_3, project6_4, project6_5,
+  project6_6, project6_7
+];
+
+export const projects = [
+  {
+    title: "Inground Pool Installation",
+    preview: project1Preview,
+    images: project1Images
+  },
+  {
+    title: "Outdoor Kitchen and BBQ Area",
+    preview: project2Preview,
+    images: project2Images
+  },
+  {
+    title: "Restaurant Interior Stone Wall",
+    preview: project3Preview,
+    images: project3Images
+  },
+  {
+    title: "Stone Fire Pit and Patio",
+    preview: project4Preview,
+    images: project4Images
+  },
+  {
+    title: "Garden Stone Wall and Pathway",
+    preview: project5Preview,
+    images: project5Images
+  },
+  {
+    title: "Cobblestone Driveway Installation",
+    preview: project6Preview,
+    images: project6Images
+  }
+];
